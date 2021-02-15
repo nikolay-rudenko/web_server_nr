43445fa640aa65c9c4f54aea7a5b36450c48463a
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-02-15 20:39:19 +0200

    added icon, creating form, connecting form

--- portfolio.py
+++ portfolio.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, url_for, redirect, render_template
+import csv
 
 app = Flask(__name__)
 
@@ -10,13 +11,35 @@ def home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('db.txt', 'a') as db1:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+
+        db1.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('db.csv', 'a', newline='') as db2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+
+        csv_writer = csv.writer(db2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        print(data)
-        return redirect('thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to db'
     else:
         return 'something went wrong'
+
+
 
 
